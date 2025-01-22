848eb6cb53223aaa1460e0252a5d586da8ae0fbd
Voravich S <vsilapac@gmail.com>
2025-01-22 02:48:55 -0500

    rudimentary scrolling text, text scripting, and improved planet view

---
+++ Scenes/DialogUI.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var dialog_line = %DialogLine
+
+const ANIMATION_SPEED : int = 20
+
+var animate_text : bool = false
+var current_visible_chars : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if animate_text:
+		if dialog_line.visible_ratio < 1:
+			dialog_line.visible_ratio += (1.0/dialog_line.text.length()) * (ANIMATION_SPEED * delta)
+			current_visible_chars = dialog_line.visible_characters
+		else:
+			animate_text = false
+
+func change_line(line: String):
+	current_visible_chars = 0
+	dialog_line.visible_characters = 0
+	dialog_line.text = line
+	animate_text = true
--- Scripts/DebugStage.gd
+++ Scripts/DebugStage.gd
@@ -1,12 +1,42 @@
 extends Node2D
 
 @onready var debug_planet: AnimatedSprite2D = $UICanvasLayer/Planet/DebugPlanet
+@onready var dialog_ui = %DialogUI
+
+var dialog_index : int = 0
+
+const dialog_lines : Array[String] = [
+	"AI: Pog pog lmao",
+	"AI: 2",
+	"AI: 3",
+	"AI: 4"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Play planet animation
 	debug_planet.play()
+	# Process first line of dialog
+	dialog_index = 0
+	process_current_line()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("next_line"):
+		if dialog_index < len(dialog_lines) - 1:
+			dialog_index += 1
+			process_current_line()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Splits line into speaker and dialog
+func parse_line(line: String):
+	var line_info = line.split(":")
+	assert(len(line_info) >= 2)
+	return {
+		"speaker": line_info[0],
+		"dialog_line": line_info[1]
+	}
+
+# Sets dialog in UI
+func process_current_line():
+	var line = dialog_lines[dialog_index]
+	var line_info = parse_line(line)
+	dialog_ui.change_line(line_info["speaker"] + ": " + line_info["dialog_line"])
