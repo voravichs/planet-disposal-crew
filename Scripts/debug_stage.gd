0ae1b977d152606987a47780e38b2444406e2424
Voravich S <vsilapac@gmail.com>
2025-01-30 04:03:33 -0500

    decision ui start

--- Scripts/debug_stage.gd
+++ Scripts/debug_stage.gd
@@ -14,10 +14,13 @@ extends Node2D
 @onready var console_planet: Sprite2D = %ConsolePlanet
 @onready var gameplay_ui: Control = %GameplayUI
 @onready var console_info: Control = %ConsoleInfo
-@onready var dialog_ui = preload("res://Scenes/DialogUI.tscn")
-@onready var debug_stage_db_ref = preload("res://Scripts/state_parameter_db/debug_stage_db.gd")
 @onready var money_label: RichTextLabel = %MoneyLabel
 @onready var scan_button: Button = %ScanButton
+@onready var crew_dialogue: Button = %CrewDialogue
+@onready var decision_button: Button = %DecisionButton
+@onready var console_bg: PanelContainer = %ConsoleBG
+@onready var console_bg_top: PanelContainer = %ConsoleBGTop
+@onready var decision_ui: Control = %DecisionUI
 
 var rng = RandomNumberGenerator.new()
 var current_planet_index: int = 0
@@ -26,6 +29,12 @@ var dialog_ui_reference: DialogUI
 var planets_scanned = [false, false, false, false]
 
 const DIALOGUE_FILE = "res://Dialogues/Debug.dialogue"
+const BG_DANGER = preload("res://Assets/Themes/PanelContainer/bg_danger.tres")
+const BG_PLAIN = preload("res://Assets/Themes/PanelContainer/bg_plain.tres")
+const TOP_BG_DANGER = preload("res://Assets/Themes/PanelContainer/top_bg_danger.tres")
+const TOP_BG_PLAIN = preload("res://Assets/Themes/PanelContainer/top_bg_plain.tres")
+const DIALOG_UI = preload("res://Scenes/DialogUI.tscn")
+const DEBUG_STAGE_DB_REF = preload("res://Scripts/state_parameter_db/debug_stage_db.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,7 +44,7 @@ func _ready() -> void:
 	money_label.text = str(GameVariables.money)
 	# Init a debug DialogUI
 	var debug_dialogue_resource = load(DIALOGUE_FILE)
-	dialog_ui_reference = dialog_ui.instantiate().with_data(debug_dialogue_resource, "start")
+	dialog_ui_reference = DIALOG_UI.instantiate().with_data(debug_dialogue_resource, "start")
 	ui_canvas_layer.add_child(dialog_ui_reference)
 	dialog_ui_reference.finished_dialogue.connect(_show_gameplay_ui)
 	#DEBUG
@@ -44,7 +53,7 @@ func _ready() -> void:
 # Sets the planet in space and in the console view
 func set_planet():
 	# Space
-	current_planet = debug_stage_db_ref.PLANET_PARAMS[current_planet_index]
+	current_planet = DEBUG_STAGE_DB_REF.PLANET_PARAMS[current_planet_index]
 	debug_planet.play(current_planet.game_vars.anim_name)
 	debug_planet.modulate = Color(1, 1, 1, 1) 
 	# Console
@@ -53,6 +62,7 @@ func set_planet():
 
 func scan_planet():
 	console_info.set_console_info(current_planet)
+	scan_button.disabled = true
 
 # triggered upon clicking a planet on the tracker.
 # takes in an int index as an argument, 
@@ -77,9 +87,12 @@ func _planet_tracker_pressed(index: int) -> void:
 				scan_planet()
 			else:
 				console_info.hide_console_info()
+				scan_button.disabled = false
 		elif planet_tracker[i].disabled == false:
 			pointer_array[i].modulate = Color(1, 1, 1, 0) 
 			pointer_array[i].stop()
+			decision_button.disabled = false
+			crew_dialogue.disabled = false
 
 # sets the gameplay UI to visible, then removes the dialog UI from the tree
 func _show_gameplay_ui():
@@ -95,6 +108,9 @@ func _explode_button() -> void:
 		if current_planet_index == i:
 			planet_tracker[i].disabled = true
 			pointer_array[i].play("x")
+			decision_button.disabled = true
+			crew_dialogue.disabled = true
+			_return()
 
 # Scans the planet, then takes money out of the budget
 func _on_scan_button_pressed() -> void:
@@ -108,6 +124,20 @@ func _on_scan_button_pressed() -> void:
 func _on_crew_dialogue_pressed() -> void:
 	gameplay_ui.visible = false
 	var debug_dialogue_resource = load(DIALOGUE_FILE)
-	dialog_ui_reference = dialog_ui.instantiate().with_data(debug_dialogue_resource, current_planet.game_vars.dialogue)
+	dialog_ui_reference = DIALOG_UI.instantiate().with_data(debug_dialogue_resource, current_planet.game_vars.dialogue)
 	ui_canvas_layer.add_child(dialog_ui_reference)
 	dialog_ui_reference.finished_dialogue.connect(_show_gameplay_ui)
+
+func _on_decision_pressed() -> void:
+	var planet_tracker = [b1, b2, b3, b4]
+	if planet_tracker[current_planet_index].disabled == false:
+		console_bg.theme = BG_DANGER
+		console_bg_top.theme = TOP_BG_DANGER
+		gameplay_ui.visible = false
+		decision_ui.visible = true
+
+func _return() -> void:
+	console_bg.theme = BG_PLAIN
+	console_bg_top.theme = TOP_BG_PLAIN
+	gameplay_ui.visible = true
+	decision_ui.visible = false
